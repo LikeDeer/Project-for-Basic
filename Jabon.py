5b1400408188976fc5179a74cf8e9415eff2b7a3
JoJunHwa <jjj5306@naver.com>
2021-05-17 17:00:42 +0900

    자본변동표 수정

--- Jabon.py
+++ Jabon.py
@@ -1,10 +1,10 @@
 #coding:utf-8
-#재무상태표
+#자본변동표
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv('2020_1분기자본변동표.csv')
 df_except_row = df[(df['항목코드'] == 'dart_EquityAtBeginningOfPeriod') | (df['항목코드'] == 'ifrs-full_Equity')]
-df_print = df_except_row.loc[:,['회사명', '업종명', '결산기준일', '항목명', '자본']]
-df_print = df_print.dropna(axis=1)
+df_print = df_except_row.iloc[:,[2, 3, 5, 7, 11, 20]]
+df_print = df_print.fillna('0.0')
 print(df_print)
